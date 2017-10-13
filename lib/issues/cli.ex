4c5a98b85702d065edf0949be94e8489bc6eee51
Richard Steen <rsteen@covermymeds.com>
2017-10-13 13:49:31 -0500

    more great code

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,4 +1,5 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
 
   @default_count 4
 
@@ -8,7 +9,7 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
 
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
@@ -53,6 +54,7 @@ defmodule Issues.CLI do
     |> decode_response
     |> sort_into_ascending_order
     |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def sort_into_ascending_order(list_of_issues) do
